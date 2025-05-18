8495aeb3c77fe5f1e77c2e02a32741a9958204de
Dmitry Starchevsky <dstarchevskiy@kamaz.digital>
2025-05-18 16:21:37 +0300

    add bottom sheet

--- app/src/main/java/com/example/shrinkulator/MainActivity.kt
+++ app/src/main/java/com/example/shrinkulator/MainActivity.kt
@@ -1,169 +1,88 @@
+@file:OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalMaterial3Api::class
+)
+
 package com.example.shrinkulator
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import com.example.shrinkulator.ui.theme.ShrinkulatorTheme
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    private var langs: List<CalcModel> = emptyList()
-
-    private val myFlow: MutableStateFlow<List<CalcModel>> = MutableStateFlow(langs)
-
-    private var weightState: MutableState<String> = mutableStateOf("")
-    private var priceState: MutableState<String> = mutableStateOf("")
-
-    private val myOnClick: () -> Unit = {
-        println("@@@myOnClick")
-
-        val newList: MutableList<CalcModel> = myFlow.value.toMutableList()
-        newList.add(
-            CalcModel(
-                weight = weightState.value,
-                price = priceState.value,
-                calc = ""
-            )
-        )
-        myFlow.value = newList
-        weightState.value = ""
-        priceState.value = ""
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
         setContent {
-            weightState = remember { mutableStateOf("") }
-
             ShrinkulatorTheme {
-                MyLayout(
-                    myFlow = myFlow,
-                    myOnClick = myOnClick,
-                    weightState = weightState,
-                    priceState = priceState
-                )
+                MyScaffold()
             }
         }
     }
-}
 
-@Composable
-fun InputForm(
-    weightState: MutableState<String>,
-    priceState: MutableState<String>,
-) {
-    Box(modifier = Modifier.fillMaxWidth()
-        .background(color = Color.LightGray)
-        .padding(all = 12.dp)
-    ) {
-        Row {
-            TextField(
-                value = weightState.value,
-                placeholder = {
-                    Text(text = "Weight")
-                },
-                modifier = Modifier.weight(1f)
-                    .padding(2.dp),
-
-                onValueChange = {
-                    println("onChange: $it")
-                    weightState.value = it
-                }
-            )
-
-            TextField(
-                value = priceState.value,
-                placeholder = {
-                    Text(text = "Price")
-                },
-                modifier = Modifier.weight(1f)
-                    .padding(2.dp),
-                onValueChange = {
-                    println("onChange: $it")
-                    priceState.value = it
-                }
-            )
-        }
-    }
-}
-
-@Composable
-fun MyLayout(
-    myFlow: StateFlow<List<CalcModel>>,
-    myOnClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    weightState: MutableState<String>,
-    priceState: MutableState<String>,
-) {
-    val collected: State<List<CalcModel>> = myFlow.collectAsState()
-
-    Column(
-        modifier = modifier.background(color = Color.Blue)
-            .fillMaxSize()
-
-    ) {
-        LazyColumn(
-            modifier = modifier.weight(1f),
-        ) {
-            itemsIndexed(
-                items = collected.value,
-                itemContent = { index, value ->
-                    Text(text = "$index ${value.weight} / ${value.price} ")
-                }
-            )
+    @Composable
+    fun MyScaffold() {
+        val sheetState = rememberModalBottomSheetState()
+        val scope = rememberCoroutineScope()
+        var showBottomSheet by remember {
+            mutableStateOf(false)
         }
 
-        Box(modifier = Modifier.fillMaxWidth()
-            .background(color = Color.Gray)
-            .padding(all = 12.dp)
-        ) {
-            Column {
-                InputForm(
-                    weightState = weightState,
-                    priceState = priceState
+        Scaffold(
+            floatingActionButton = {
+                ExtendedFloatingActionButton(
+                    text = { Text("Show bottom sheet") },
+                    icon = { Icon(Icons.Filled.Add, contentDescription = "") },
+                    onClick = {
+                        showBottomSheet = true
+                    }
                 )
+            }
+        ) { contentPadding ->
+            // Screen content
 
-                Button(
-                    onClick = myOnClick,
-                    modifier = Modifier.fillMaxWidth()
+            if (showBottomSheet) {
+                ModalBottomSheet(
+                    onDismissRequest = {
+                        showBottomSheet = false
+                    },
+                    sheetState = sheetState
                 ) {
-                    Text(text = "ADD")
+                    // Sheet content
+                    Button(onClick = {
+                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                            if (!sheetState.isVisible) {
+                                showBottomSheet = false
+                            }
+                        }
+                    }) {
+                        Text("Hide bottom sheet")
+                    }
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShrinkulatorTheme {
-//        MyLayout(myState = State<List>(emptyList()), langs = listOf("ASD","QWER","QWER"), {})
-    }
-}
+
