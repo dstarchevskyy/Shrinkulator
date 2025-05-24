bb89bc72543d9313030509500f4f8890abb32f6e
Dmitry Starchevsky <dstarchevskiy@kamaz.digital>
2025-05-24 22:21:03 +0300

    add ModalNavigationDrawer

--- app/src/main/java/com/example/shrinkulator/MainActivity.kt
+++ app/src/main/java/com/example/shrinkulator/MainActivity.kt
@@ -9,13 +9,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -25,6 +32,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.shrinkulator.ui.theme.ShrinkulatorTheme
 import kotlinx.coroutines.launch
 
@@ -36,7 +47,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShrinkulatorTheme {
-                MyScaffold()
+                ModalNavigationDrawer(
+                    drawerContent = {
+                        ModalDrawerSheet {
+                            Text("Drawer title", modifier = Modifier.padding(16.dp))
+                            HorizontalDivider()
+                            NavigationDrawerItem(
+                                label = { Text(text = "Drawer Item") },
+                                selected = false,
+                                onClick = { /*TODO*/ }
+                            )
+                            Image(
+                                modifier = Modifier.fillMaxSize(),
+                                painter = painterResource(R.drawable.ic_launcher_foreground),
+                                contentDescription = "",
+                                contentScale = ContentScale.Fit)
+                            // ...other drawer items
+                        }
+                    },
+                ) {
+                    MyScaffold()
+                }
             }
         }
     }
